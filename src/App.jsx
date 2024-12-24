79fbeb9df08052d740600d0386b3594fc775299b
NEVERTHELESSE <neverthelesse@localhost.localdomain>
2024-12-24 17:10:18 +0100

    all

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './index.css'
+import React from 'react'
+
+function App() {
+  return (
+    <div className='bg-orange-500 h-[100vh] text-[10rem]'>
+      <h1>hello from neverthelesse</h1>
+      <h1>for kaftans</h1>
+      
+    </div>
+  )
+}
+
+export default App
